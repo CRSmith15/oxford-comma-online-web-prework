9ce47ff2dae59007d824901203907510ebbdf070
Chance Smith <chancesmith97@hotmail.com>
2019-02-04 19:21:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     pos = 2
     [array[0...pos], array[pos..-1]].map { |x| x.join ', ' }.join(', and ')
   else array.length > 3
-    array.join(", ") << array.insert("and ", -1).join
+    array.join(", ") << array.insert("and ", -1)
 
 
   end
